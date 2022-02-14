bf1246240d2d09b04be803db4a5d1055eed0a6be
lorenzoc25 <76075882+lorenzoc25@users.noreply.github.com>
2022-02-13 19:07:04 -0800

    Add link to programming notes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,18 +26,28 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || 'Hi, my name is '} <span className="text-color-main">{name}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
+          <p className="hero-cta" display="inline">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
                 {cta || 'Know more'}
               </Link>
+            </span>
+          </p>
+          <p className="hero-cta" display="inline">
+            <span className="cta-btn cta-btn--hero">
+              <text
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://sable-sturgeon-607.notion.site/Computer-Science-9237617609494ae3920efa825ffa2938"
+              >
+                Programming Notes
+              </text>
             </span>
           </p>
         </Fade>
